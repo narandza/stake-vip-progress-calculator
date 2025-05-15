bf3ab6ed2e3341e0f34141edaca41f712927693d
narandza <60319154+narandza@users.noreply.github.com>
2025-05-15 19:05:38 +0200

    upade logo

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div className="">Stake VIP Calculator</div>;
+  return (
+    <div className="h-full flex items-center justify-center">
+      <Image
+        src="/logo.svg"
+        alt="Logo"
+        height={128}
+        width={128}
+        className="invert mt-6"
+      />
+    </div>
+  );
 }
