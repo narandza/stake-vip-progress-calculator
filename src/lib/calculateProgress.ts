ae9d8f44ebea0ce99b217b4e20054a53d5f44aa4
narandza <60319154+narandza@users.noreply.github.com>
2025-05-16 06:37:48 +0200

    update vip tiers

--- src/lib/calculateProgress.ts
+++ src/lib/calculateProgress.ts
@@ -1,16 +1,22 @@
-type VIPLevel = {
-    name: string;
-    wagererRequired: number;
-}
+type VipTier = {
+  name: string;
+  requirement: number; // in USD
+};
 
-const VIP_TIERS: VIPLevel = [{
- { name: "Bronze", wagererRequired: 10 },
-  { name: "Silver", wagererRequired: 2500 },
-  { name: "Gold", wagererRequired: 10000 },
-  { name: "Platinum I", wagererRequired: 25000 },
-  { name: "Platinum II", wagererRequired: 50000 },
-  { name: "Platinum III", wagererRequired: 100000 },
-  { name: "Platinum IV", wagererRequired: 250000 },
-  { name: "Platinum V", wagererRequired: 500000 },
-  { name: "Diamond", wagererRequired: 1000000 },
-}]
+const VIP_TIERS: VipTier[] = [
+  { name: "Bronze", requirement: 10_000 },
+  { name: "Silver", requirement: 50_000 },
+  { name: "Gold", requirement: 100_000 },
+  { name: "Platinum I", requirement: 250_000 },
+  { name: "Platinum II", requirement: 500_000 },
+  { name: "Platinum III", requirement: 1_000_000 },
+  { name: "Platinum IV", requirement: 2_500_000 },
+  { name: "Platinum V", requirement: 5_000_000 },
+  { name: "Platinum VI", requirement: 10_000_000 },
+  { name: "Diamond I", requirement: 25_000_000 },
+  { name: "Diamond II", requirement: 50_000_000 },
+  { name: "Diamond III", requirement: 100_000_000 },
+  { name: "Diamond IV", requirement: 250_000_000 },
+  { name: "Diamond V", requirement: 500_000_000 },
+  { name: "Obsidian", requirement: 1_000_000_000 },
+];
