f635cd19d0d4cf17f27fac4aee7d1a8ccb1fbb84
narandza <60319154+narandza@users.noreply.github.com>
2025-05-28 01:30:25 +0200

    build form schema; build form hook

--- src/modules/calculate-progress-form.tsx
+++ src/modules/calculate-progress-form.tsx
@@ -1,4 +1,7 @@
 import { VipTier } from "@/constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 interface CalculateProgressFormProps {
   tiers: VipTier[];
@@ -7,6 +10,23 @@ interface CalculateProgressFormProps {
 export const CalculateProgressForm = ({
   tiers,
 }: CalculateProgressFormProps) => {
+  const formSchema = z.object({
+    currentPercentage: z.number(),
+    tier: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      currentPercentage: 0,
+      tier: "none",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <div className="">
       {tiers.map((tier) => (
