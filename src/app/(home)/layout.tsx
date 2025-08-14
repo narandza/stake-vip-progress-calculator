ab78cc08f77c2ffb85729b819f21309387bb7d2f
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-14 20:55:03 +0200

    fix errors

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,6 +2,8 @@ interface Props {
   children: React.ReactNode;
 }
 
-const Layout = ({ children }) => {
+const Layout = ({ children }: Props) => {
   return <main className="">{children}</main>;
 };
+
+export default Layout;
