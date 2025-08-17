7ba1e450d9482cc819826fed242fc5e8d025059e
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-08-17 03:26:32 +0200

    build a footer

--- src/modules/home/ui/components/footer.tsx
+++ src/modules/home/ui/components/footer.tsx
@@ -1,3 +1,10 @@
 export const Footer = () => {
-  return <footer className="">Footer</footer>;
+  return (
+    <footer className="border-t-2 w-full flex items-center justify-center h-[8vh]">
+      <p className="text-xs text-muted-foreground text-center">
+        Disclaimer: This site is for informational purposes only and is not
+        associated with, authorized, or endorsed by Stake.com.
+      </p>
+    </footer>
+  );
 };
