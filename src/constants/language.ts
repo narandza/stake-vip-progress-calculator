ae36045d638648e9f0e630b523b99bdc6810fa3a
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-10-02 19:06:37 +0200

    add language options cons

--- src/constants/language.ts
+++ src/constants/language.ts
@@ -2,3 +2,14 @@ import z from "zod";
 
 export const LanguageEnum = z.enum(["en", "es"]);
 export type LanguageType = z.infer<typeof LanguageEnum>;
+
+export const languageOptions: { value: LanguageType; label: string }[] = [
+  {
+    value: "en",
+    label: "English",
+  },
+  {
+    value: "es",
+    label: "Español",
+  },
+];
