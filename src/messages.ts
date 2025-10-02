2c3a24af83d3e3f6b009292e4ba4b1233abb66d3
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-10-02 18:43:09 +0200

    add article constant

--- src/messages.ts
+++ src/messages.ts
@@ -1,5 +1,7 @@
 import { LanguageType } from "./constants";
 
+const ARTICLE = "<strong><em>ADD_ARTICLE<em/><strong>";
+
 interface MessageParams {
   tier: string;
   nextTier: string;
@@ -21,7 +23,7 @@ export const MESSAGES: MessageTemplates = {
   Sports bets count three times more toward your wagering requirements. For example, to work out how many sports bets would get you to the next level, just divide the remaining amount by 3.<br/><br/>
 
   If you’re placing a mix of sports and casino bets, the best way to keep track of your progress is through your transaction history. We’ve also prepared a quick guide so you can calculate it yourself anytime:<br/>
-  ADD_ARTICLE<br/><br/>
+  ${ARTICLE}<br/><br/>
 
 
   If you have any questions or just want an update along the way, we’re here for you 24/7. Keep up the great progress!
@@ -38,7 +40,7 @@ export const MESSAGES: MessageTemplates = {
   Las apuestas deportivas cuentan tres veces más para cumplir con tus requisitos de apuesta. Por ejemplo, para calcular cuántas apuestas deportivas necesitarías para llegar al siguiente nivel, solo divide la cantidad restante entre 3.<br/><br/>
   
   Si realizas una combinación de apuestas deportivas y de casino, la mejor manera de seguir tu progreso es revisando tu historial de transacciones. También hemos preparado una guía rápida para que puedas calcularlo tú mismo en cualquier momento:<br/>
-  ADD_ARTICLE<br/><br/>
+  ${ARTICLE}<br/><br/>
   
   Si tienes alguna otra pregunta o quieres una actualización de tu progreso, estamos disponibles para ayudarte las 24 horas, los 7 días de la semana. Sigue así, estás muy cerca.
   `,
