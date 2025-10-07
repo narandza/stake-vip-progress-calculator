42bf5e38f5017591196ff3a992a846086412f426
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-10-07 13:11:25 +0200

    change language order

--- src/constants/language.ts
+++ src/constants/language.ts
@@ -3,25 +3,10 @@ import z from "zod";
 export const LanguageEnum = z.enum(["en", "es", "ar", "tr", "fr"]);
 export type LanguageType = z.infer<typeof LanguageEnum>;
 
-export const languageOptions: { value: LanguageType; label: string }[] = [
-  {
-    value: "en",
-    label: "English",
-  },
-  {
-    value: "es",
-    label: "Español",
-  },
-  {
-    value: "ar",
-    label: "العربية",
-  },
-  {
-    value: "tr",
-    label: "Türkçe",
-  },
-  {
-    value: "fr",
-    label: "Français",
-  },
+export const languageOptions = [
+  { value: "en", label: "English" },
+  { value: "es", label: "Español" },
+  { value: "fr", label: "Français" },
+  { value: "tr", label: "Türkçe" },
+  { value: "ar", label: "العربية" },
 ];
