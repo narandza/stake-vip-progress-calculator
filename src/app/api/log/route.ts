344490231f1fdbe4e79cf69e60f21f304dd6c63f
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-10-17 19:38:37 +0200

    fix type saftey

--- src/app/api/log/route.ts
+++ src/app/api/log/route.ts
@@ -2,10 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { logUsage } from "@/lib/logUsage";
 import { LanguageType } from "@/modules/constants/language";
+import { LOG_TYPE } from "@/modules/constants/constants";
 
 export async function POST(req: NextRequest) {
   const { type, language } = (await req.json()) as {
-    type: "visit" | "calculate" | "copy";
+    type: LOG_TYPE;
     language: LanguageType;
   };
 
