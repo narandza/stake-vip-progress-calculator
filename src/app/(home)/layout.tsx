09b5dd7d87d73b0d4a2613f7bc229c90a3e1e08d
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2025-10-17 19:32:14 +0200

    fix layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   return (
-    <main className="flex flex-col min-h-screen bg-background text-foreground">
+    <main className="flex flex-col min-h-screen bg-background text-foreground gap-y-10">
       <Header />
-      <div className="flex-1 flex justify-center pt-10">
+      <div className="flex-1 flex justify-center">
         <div className="w-full max-w-xs">{children}</div>
       </div>
       <Footer />
